c89e4cc86587effd971be372f46519e6066bc63e
Chris Bailey <chris@ghostly.io>
2024-09-23 02:23:31 -0400

    base lucia

---
+++ src/lib/auth.ts
@@ -0,0 +1,28 @@
+import { DrizzlePostgreSQLAdapter } from "@lucia-auth/adapter-drizzle";
+import { sessions, users } from "./database/schema";
+import { db } from "./database";
+import { Lucia } from "lucia";
+
+const adapter = new DrizzlePostgreSQLAdapter(db, sessions, users);
+
+export const lucia = new Lucia(adapter, {
+  sessionCookie: {
+    attributes: {
+      secure: process.env.NODE_ENV === "production",
+    },
+  },
+  getUserAttributes: (attributes) => {
+    return {
+      email: attributes.email,
+    };
+  },
+});
+
+declare module "lucia" {
+  interface Register {
+    Lucia: typeof lucia;
+    DatabaseUserAttributes: {
+      email: string;
+    };
+  }
+}
