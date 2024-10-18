c044e687a5f1739c46526ffe3f3b7cf99d7ca6a4
Chris Bailey <chris@ghostly.io>
2024-10-18 00:36:18 -0400

    🐛 feat(profile): update ProfilePracticeCard to format play date and add unit tests

--- src/app/profile/_components/profile-practice-card.tsx
+++ src/app/profile/_components/profile-practice-card.tsx
@@ -1,4 +1,3 @@
-import { Badge } from '@/components/ui/badge';
 import { Practice } from '@/shared/types';
 
 type Props = {
@@ -10,20 +9,19 @@ export function ProfilePracticeCard({ practice }: Props) {
     <li key={practice.id} className="flex justify-between items-start">
       <div>
         <p className="font-medium">{practice.type}</p>
-        <p className="text-sm text-muted-foreground">{practice.playDate}</p>
+        <p className="text-sm text-muted-foreground">
+          {new Date(practice.playDate).toLocaleDateString()}
+        </p>
         <p className="text-sm">{`${practice.location}, ${practice.city}, ${practice.state}`}</p>
       </div>
-      <div className="text-right">
+      {/* <div className="text-right">
         <Badge variant="outline">
-          {new Date(practice.playDate).toLocaleTimeString([], {
-            hour: '2-digit',
-            minute: '2-digit',
-          })}
+          {new Date(practice.playDate).toLocaleDateString()}
         </Badge>
         {practice.notes && (
           <p className="mt-1 text-xs text-muted-foreground">{practice.notes}</p>
         )}
-      </div>
+      </div> */}
     </li>
   );
 }
