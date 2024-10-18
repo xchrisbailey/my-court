32c42a340d747462d0ea64752b4ea03f5afeec1d
Chris Bailey <chris@ghostly.io>
2024-10-18 00:56:41 -0400

    feat(profile): refactor ProfilePage to use ProfileView component and add loading skeleton

---
+++ src/app/profile/_components/profile-view.tsx
@@ -0,0 +1,100 @@
+'use client';
+
+import { ProfileGearSetCard } from '@/app/profile/_components/profile-gear-set-card';
+import { ProfileMatchCard } from '@/app/profile/_components/profile-match-card';
+import { ProfilePracticeCard } from '@/app/profile/_components/profile-practice-card';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { UserWithRelations } from '@/shared/types';
+import { Badge, Dumbbell, ShoppingBag, Trophy } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { use } from 'react';
+
+type Props = {
+  userPromise: Promise<UserWithRelations | undefined>;
+};
+
+export function ProfileView({ userPromise }: Props) {
+  const user = use(userPromise);
+  const router = useRouter();
+  if (!user) {
+    router.push('/login');
+  }
+
+  return (
+    <div className="container p-4 mx-auto space-y-6">
+      <Card>
+        <CardContent className="flex items-center pt-6 space-x-4">
+          <Avatar className="w-24 h-24">
+            <AvatarImage src={'/robotar.svg'} alt={'chris bailey'} />
+            <AvatarFallback>
+              {'chris bailey'
+                .split(' ')
+                .map(n => n[0])
+                .join('')}
+            </AvatarFallback>
+          </Avatar>
+          <div>
+            <h1 className="text-2xl font-bold">Chris Bailey</h1>
+            {/* <p className="text-muted-foreground">@{user.username}</p> */}
+            <Badge className="mt-2">Rank #1</Badge>
+          </div>
+        </CardContent>
+      </Card>
+
+      <div className="grid gap-6 md:grid-cols-2">
+        <Card>
+          <CardHeader>
+            <CardTitle className="flex items-center">
+              <Trophy className="mr-2" />
+              Recent Matches
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <ul className="space-y-4">
+              {user.matches.map(match => (
+                <ProfileMatchCard match={match} key={match.id} />
+              ))}
+            </ul>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <CardTitle className="flex items-center">
+              <Dumbbell className="mr-2" />
+              Recent Practices
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <ul className="space-y-4">
+              {user.practices.map(practice => (
+                <ProfilePracticeCard practice={practice} key={practice.id} />
+              ))}
+            </ul>
+          </CardContent>
+        </Card>
+      </div>
+
+      <Card>
+        <CardHeader>
+          <CardTitle className="flex items-center">
+            <ShoppingBag className="mr-2" /> My Gear
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <ul className="space-y-4">
+            {user.gearSets.map(gearSet => (
+              <ProfileGearSetCard gearSet={gearSet} key={gearSet.id} />
+            ))}
+          </ul>
+        </CardContent>
+      </Card>
+
+      <div className="flex justify-center">
+        <Button>Edit Profile</Button>
+      </div>
+    </div>
+  );
+}
