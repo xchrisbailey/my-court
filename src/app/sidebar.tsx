672a77c79b55bca2e9f63ef08d3b998e08d53e41
Chris Bailey <chris@ghostly.io>
2024-10-19 00:18:42 -0400

    ✨feat(sidebar): switch to new shadcn sidebar

---
+++ src/app/sidebar.tsx
@@ -0,0 +1,164 @@
+'use client';
+
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { Separator } from '@/components/ui/separator';
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarInset,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+  SidebarProvider,
+  SidebarTrigger,
+} from '@/components/ui/sidebar';
+import { LogOut, Trophy, User } from 'lucide-react';
+import Link from 'next/link';
+import * as React from 'react';
+
+// This is sample data.
+const data = {
+  navMain: [
+    {
+      title: 'Gear',
+      url: '#',
+      items: [
+        {
+          title: 'Rackets',
+          url: '/rackets',
+          isActive: true,
+        },
+        {
+          title: 'Strings',
+          url: '/strings',
+        },
+        {
+          title: 'Brands',
+          url: '/brands',
+        },
+      ],
+    },
+    {
+      title: 'Play',
+      url: '#',
+      items: [
+        {
+          title: 'Matches',
+          url: '/matches',
+        },
+        {
+          title: 'Practices',
+          url: '/practices',
+        },
+      ],
+    },
+  ],
+};
+
+export default function SideBarView({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <SidebarProvider
+      style={
+        {
+          '--sidebar-width': '19rem',
+        } as React.CSSProperties
+      }
+    >
+      <Sidebar variant="floating">
+        <SidebarHeader>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton size="lg" asChild>
+                <a href="#">
+                  <div className="flex justify-center items-center rounded-lg aspect-square size-8 bg-sidebar-primary text-sidebar-primary-foreground">
+                    <Trophy className="size-4" />
+                  </div>
+                  <div className="flex flex-col gap-0.5 leading-none">
+                    <span className="font-semibold">my court</span>
+                  </div>
+                </a>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarHeader>
+        <SidebarContent>
+          <SidebarGroup>
+            <SidebarMenu className="gap-2">
+              {data.navMain.map(item => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url} className="font-medium">
+                      {item.title}
+                    </a>
+                  </SidebarMenuButton>
+                  {item.items?.length ? (
+                    <SidebarMenuSub className="px-1.5 ml-0 border-l-0">
+                      {item.items.map(item => (
+                        <SidebarMenuSubItem key={item.title}>
+                          <SidebarMenuSubButton
+                            asChild
+                            isActive={item.isActive}
+                          >
+                            <a href={item.url}>{item.title}</a>
+                          </SidebarMenuSubButton>
+                        </SidebarMenuSubItem>
+                      ))}
+                    </SidebarMenuSub>
+                  ) : null}
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroup>
+        </SidebarContent>
+      </Sidebar>
+      <SidebarInset>
+        <header className="flex justify-between items-center px-4 h-16 shrink-0">
+          <div className="flex gap-2 items-center">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="h-6" />
+            <h1 className="text-xl font-semibold">My Court</h1>
+          </div>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Avatar className="cursor-pointer">
+                <AvatarImage
+                  src="/placeholder.svg?height=32&width=32"
+                  alt="User avatar"
+                />
+                <AvatarFallback>U</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem>
+                <User className="mr-2 w-4 h-4" />
+                <Link href="/profile">View Profile</Link>
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <LogOut className="mr-2 w-4 h-4" />
+                <span>Log out</span>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </header>
+        {children}
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
